248275da84645c1496072b55c3e343d81675c728
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-10-10 20:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var answer2 = [];
 function johnLennonFacts(facts){
   while(counter < facts.length){
     answer2[counter] = facts[counter] + "!!!"
-    console.log(facts[counter])
+    console.log(facts[counter]);
     counter++
   }
   return answer2;
