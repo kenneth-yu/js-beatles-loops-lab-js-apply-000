ea7e4d7cb76c7bfc8a46d048d13d49c18fe9cf80
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-10-10 20:37:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function theBeatlesPlay(musicians, instruments){
 
 var counter = 0;
 var answer2 = [];
-function johnLennonFacts(){
+function johnLennonFacts(facts){
   while(counter < facts.length){
     answer2[counter] = facts[counter] + "!!!"
     counter++
