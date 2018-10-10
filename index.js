e1aac66871addf3b969846a4f23356c4ea803916
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-10-10 20:01:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var musicians[]
 var instruments[]
 var array[]
 function theBeatlesPlay(musicians, instruments){
+  if (musicians.length() > instruments.length())
   for(i = 0; i<musicians.length();i++){
     array[i] = musicians[i] + instruments[i];
   }
